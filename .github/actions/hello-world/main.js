2ebb0a2bd2e9b00eef2d961f825a4e512990e62a
Luke Badgerow <badgerow.luke@gmail.com>
2021-08-16 15:17:40 -0400

    so it was syntax issue on the yaml

--- .github/actions/hello-world/main.js
+++ .github/actions/hello-world/main.js
@@ -1,11 +1,12 @@
-const core = require("@actions/core")
-const fistGreeting = core.getInput("first-greeting")
-const secondGreeting = core.getInput("second-greeting")
-const thirdGreeting = core.getInput("third-greeting")
+const core = require("@actions/core");
+const fistGreeting = core.getInput("first-greeting");
+const secondGreeting = core.getInput("second-greeting");
+const thirdGreeting = core.getInput("third-greeting");
 
 
-console.log(`Hello ${fistGreeting}`)
-console.log(`Hello ${secondGreeting}`)
+console.log(`Hello ${fistGreeting}`);
+console.log(`Hello ${secondGreeting}`);
+
 if(thirdGreeting) {
-    console.log(`Hello ${thirdGreeting}`)
+    console.log(`Hello ${thirdGreeting}`);
 }
