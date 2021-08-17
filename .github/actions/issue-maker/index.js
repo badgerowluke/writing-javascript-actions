936d019a48aef08bb9b5c649fc38b7c638217617
Luke Badgerow <badgerow.luke@gmail.com>
2021-08-17 09:43:32 -0400

    slow should be fast

--- .github/actions/issue-maker/index.js
+++ .github/actions/issue-maker/index.js
@@ -1,6 +1,6 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
-const octo = require("@octokit/rest");
+const { Octokit } = require("@octokit/rest");
 
 async function run() {
     try {
